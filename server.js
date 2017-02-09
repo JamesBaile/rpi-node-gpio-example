e2d6d4f34f09812e40cd8e8de3c716783abf0581
James Baile <james.baile@comparethemarket.com>
2017-02-09 21:56:23 +0000

    Amended to use rpio node module

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-var gpio = require('rpi-gpio');
+var rpio = require('rpio');
 
-gpio.setup(11, gpio.DIR_OUT, write);
+/*
+ * Set the initial state to low.  The state is set prior to the pin becoming
+ * active, so is safe for devices which require a stable setup.
+ */
+rpio.open(11, rpio.OUTPUT, rpio.LOW);
 
-function write() {
-    gpio.write(11, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
+/*
+ * The sleep functions block, but rarely in these simple programs does one care
+ * about that.  Use a setInterval()/setTimeout() loop instead if it matters.
+ */
+for (var i = 0; i < 5; i++) {
+        /* On for 1 second */
+        rpio.write(11, rpio.HIGH);
+        rpio.sleep(1);
+
+        /* Off for half a second (500ms) */
+        rpio.write(11, rpio.LOW);
+        rpio.msleep(500);
 }
